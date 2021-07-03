c205f69d02af4e09f6ec0df907e3d902a9aff9c7
Jiwon <jrimit@gmail.com>
2021-07-03 13:29:16 +0900

    Revert "양재호"

--- ideathone/main/urls.py
+++ ideathone/main/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 from .views import *
-from django.conf.urls.static import static
-from django.conf import settings
 
 app_name = 'main'
 
 urlpatterns=[
-    path('', main, name = 'main')
+    
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
